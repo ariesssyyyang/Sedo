7df7f71f1a6f9d9508cd1bfec887c9e10dc7bec2
ariesssyyyang <ariesssyyyang@gmail.com>
2017-12-14 12:31:18 +0800

    install framework

---
+++ Sedo/Sedo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sedo
+//
+//  Created by Aries Yang on 2017/12/14.
+//  Copyright © 2017年 Aries Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
